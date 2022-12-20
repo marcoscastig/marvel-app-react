b93329424b9fadaf1f592aabd653d5a5c081bbfb
marcoscastig <1castiglionimarcos@gmail.com>
2022-12-20 19:32:26 -0300

    Final Commit

--- src/pages/Comics.js
+++ src/pages/Comics.js
@@ -1,10 +1,33 @@
-import 'semantic-ui-css/semantic.min.css'
-
+import { useState } from "react";
+import { Grid, Header } from "semantic-ui-react";
+import Container from "../components/Container";
+import ListComics from "../components/ListComics";
+import useFetch from "../hooks/useFetch";
 
 export default function Comics() {
-   return(<div>Hola desde comics
-   <p>
-    sdgfsdf
-    </p>
-    </div>) 
+  const [renderComics, setRenderComics] = useState(10);
+
+  const listComics = useFetch(
+    `http://gateway.marvel.com/v1/public/comics?ts=1&apikey=c4ec0b8b17c2e90b2c18e16a054044b6&hash=b5e740944246b9b723bc2140788a3578&orderBy=focDate&limit=${renderComics}`
+  );
+
+  
+
+  return (
+    <div className="comics-page">
+      <div id="slide-comics-image" />
+      <Grid>
+        <Grid.Column>
+          <Container bg="light">
+            <Header as="h2">Los mejores Cómics</Header>
+            <ListComics
+              listComics={listComics}
+              renderComics={renderComics}
+              setRenderComics={setRenderComics}
+            />
+          </Container>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
 }
--- src/pages/Series.js
+++ src/pages/Series.js
@@ -1,10 +1,27 @@
+import { Grid, Header } from "semantic-ui-react";
+import Container from "../components/Container";
+import ListSeries from "../components/ListSeries"; 
+import useFetch from "../hooks/useFetch";
+
+
 
 export default function Series() {
-    return(
-        <div>
-            <p>
-                Desde series
-            </p>
-        </div>
-    )
+    const listSeries = useFetch(
+      `http://gateway.marvel.com/v1/public/series?ts=1&apikey=c4ec0b8b17c2e90b2c18e16a054044b6&hash=b5e740944246b9b723bc2140788a3578&orderBy=startYear&limit=20`
+    );
+return (
+    <div>
+      <div className="series-page">
+        <div id="slide-series-image" />
+        <Grid>
+          <Grid.Column>
+            <Container bg="light">
+              <Header as="h2">Las últimas series de Marvel</Header>
+              <ListSeries listSeries={listSeries} />
+            </Container>
+          </Grid.Column>
+        </Grid>
+      </div>
+    </div>
+  );
 }
